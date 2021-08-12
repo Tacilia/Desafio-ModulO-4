dab589255115d9e60e03396453bdae578494d728
tacilia.almeida <tacilia@icloud.com>
2021-08-12 14:39:48 -0300

    Criar projeto ecomerce

---
+++ EcomerceZup/src/main/java/br/com/zup/EcomerceZup/EcomerceZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.EcomerceZup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceZupApplication.class, args);
+	}
+
+}
